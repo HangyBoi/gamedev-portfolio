bebe3effee9dd02bb2a9d3c8789ddd1b6f4dbde2
Nichita Cebotari <6avuha228@gmail.com>
2025-12-08 20:33:52 +0100

    Chore: Nothing here

--- src/App.jsx
+++ src/App.jsx
@@ -17,8 +17,8 @@ const Hero = () => (
 
       {/* Main Title */}
       <h1 className="text-5xl md:text-8xl lg:text-9xl font-bold tracking-tighter mb-6 text-white leading-none">
-        GAMEPLAY <br />
-        <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#00f3ff] to-[#0066ff]">PROGRAMMER</span>
+        Я ЛЮБЛЮ <br />
+        <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#00f3ff] to-[#0066ff]">КАТЮ</span>
       </h1>
 
       {/* Subtitle - New Colors*/}
