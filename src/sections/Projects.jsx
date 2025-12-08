40e113c9ec8cddc6ca9828636a2fa1d4edfd5a0f
Nichita Cebotari <6avuha228@gmail.com>
2025-12-08 02:47:55 +0100

    Chore: Add new images to Folksroad project gallery

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -388,9 +388,11 @@ const Projects = () => {
         "Established an efficient asset pipeline for 2D artists to deliver sprites that functioned correctly within the 2.5D perspective."
       ],
       gallery: [
-        { type: 'image', url: "/gamedev-portfolio/images/folksroad/poster-studio.webp", align: "bottom" },
-        { type: 'image', url: "/gamedev-portfolio/images/folksroad/main-mara.webp", align: "center" },
+        { type: 'image', url: "/gamedev-portfolio/images/folksroad/folksroad-theme.webp", align: "center" },
+        { type: 'image', url: "/gamedev-portfolio/images/folksroad/poster-studio.webp", align: "center" },
+        { type: 'image', url: "/gamedev-portfolio/images/folksroad/main-mara.webp", align: "top" },
         { type: 'video', url: "/gamedev-portfolio/videos/folksroad/main-theme.mp4", align: "center", muted: false },
+        { type: 'image', url: "/gamedev-portfolio/images/folksroad/enemy-forces.webp", align: "center" },
         { type: 'video', url: "/gamedev-portfolio/videos/folksroad/camera-showcase.mp4", align: "center", muted: false },
         { type: 'video', url: "/gamedev-portfolio/videos/folksroad/dialogue-showcase.mp4", align: "bottom", muted: false },
         { type: 'image', url: "/gamedev-portfolio/images/folksroad/dialogue.webp", align: "center" },
